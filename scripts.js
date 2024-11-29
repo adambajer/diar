d70056394a4f8426238975bba7d5841d5ccad669
adambajer <adam.bajer@gmail.com>
2024-11-29 07:19:41 +0100

    d

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@
 // Firebase Configuration
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-        .register('/service-worker.js')
+        .register('/serviceworker.js')
         .then((registration) => {
             console.log("Service Worker registered with scope:", registration.scope);
         })
